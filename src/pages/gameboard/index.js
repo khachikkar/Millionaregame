3b0f86f254f64f103181b2c2d95bc12ca804bcad
khachikkar <khachikkarapetyan78@gmail.com>
2024-10-30 17:55:40 +0400

    updated game logic functionality added nonlosable money scheme added money blocks and thir logic added lose and win states

--- src/pages/gameboard/index.js
+++ src/pages/gameboard/index.js
@@ -2,49 +2,137 @@ import React, { useContext, useState } from "react";
 import { GameContext } from "../../context/context";
 import "./index.css";
 import QuestItem from "../../components/questItem";
+import { Link } from "react-router-dom";
+
+const winMoney = []
+const counts = []
+
 
 const GameBoard = () => {
+
+
+function handlenewgame (){
+    console.log("new game")
+    counts.length =0
+    winMoney.length =0
+}
+
+
+
   const { data } = useContext(GameContext);
 
-  console.log(data);
+//   console.log(data);
 
   const [lose, setIsLose] = useState(false)
-  const [rightAnswer, setRightAnswer] = useState([data[0]]);
+  const [win, setWin] =useState(false)
+  const [rightAnswer, setRightAnswer] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(data[0])
+
 
 const additeminright = (e)=>{
+
+
 const answ = e.target.textContent
-
-const comp = data[rightAnswer.length - 1].correctOption
-
-if(rightAnswer.length === 10){
-    alert("you win")
-    return // must show play again button and go new game
-}
+const comp = data[rightAnswer.length].correctOption
 
 if (answ === comp){
     console.log("right")
-setRightAnswer([...rightAnswer, data[0]]);
+
+
+if(rightAnswer.length === 8){
+    alert(`you win $${winMoney[winMoney.length-1] || "no money"}`)
+    setIsLose(true)
+    setWin(true)
+    return // must show play again button and go new game
+}
+
+
+
+switch(counts.length){
+    case 2:
+    winMoney.push(4000)
+    console.log(winMoney, "winmopmney")
+    break
+    case 6:
+    winMoney.push(8000)
+    console.log(winMoney, "winmopmney")
+    break
+     case 7:
+    winMoney.push(10000)
+    console.log(winMoney, "winmopmney")
+    break
+    default:
+        break
+}
+
+setRightAnswer([...rightAnswer, data[rightAnswer.length]]);
+
+setCurrentQuestion(data[rightAnswer.length+1])
+
+counts.push(1) ///
+console.log(counts, "counts")
+
 }else{
     setIsLose(true)
     console.log("wrong")
 }
 }
 
-console.log(rightAnswer, ">>>>>>>>>")
 
-const last =  data[rightAnswer.length-1]
+
+// let last =  data[rightAnswer.length || 0]
+
+// handle Change 
+// const handleChange = () => {
+
+//     // last = data[rightAnswer.length + 1]
+// setCurrentQuestion(data[rightAnswer.length+1])
+
+
+// }
 
   return (
     <div className="GameBoardCont">
-      <h1>Game Board</h1>
+      {/* <h1>Game Board</h1> */}
 
+    <div className="points">
+    <h1>Game Borad</h1>
+    {/* <p>{rightAnswer.length}</p> */}
+
+
+
+    <div className="pointsCont">
+          {["1000 AMD", "2000 AMD", "4000 AMD", "5000 AMD", "6000 AMD", "7000 AMD", "8000 AMD", "9000 AMD", "10000 AMD"].map((amount, index) => (
+            <div
+              key={index}
+              className={`pItem ${index < rightAnswer.length ? "highlight" : ""}`}
+            >
+              {amount}
+            </div>
+          ))}
+        </div>
+
+
+    </div>
+    <div>
     
     {
         lose 
-        ? <p>you lose Try Again</p>
-        : <div><p>{rightAnswer.length}</p> <QuestItem  item={last} additeminright={additeminright} /></div>
-
+        ?
+        win 
+        ? <p>Congrats You Win and you have ${winMoney[winMoney.length-1]} Dollars  <Link to="/game"><button>Start New Game</button></Link></p>
+        : <p>you lose and have ${winMoney[winMoney.length-1] || "0"} Dollars Try Again <Link to="/game"><button onClick={handlenewgame}>Try Again</button></Link></p>
+        : <div> 
+            
+        <QuestItem  item={currentQuestion} additeminright={additeminright} />
+        <div>
+            <button>Chnage a Question</button>
+            <button>Area Help</button>
+        </div>
+        
+        </div>
     }
+    </div>
 
     </div>
   );
